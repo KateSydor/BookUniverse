cff60b08fd73e0ace56323aed201b346bd605542
KateYatsiuk <katerynayatsiuk@gmail.com>
2023-09-28 14:32:42 +0300

    Removed comments

--- BookUniverse/ConsoleApp/Utils.cs
+++ BookUniverse/ConsoleApp/Utils.cs
@@ -20,14 +20,6 @@ namespace BookUniverseConsole
             Console.WriteLine("SQL script executed successfully");
         }
 
-        /*public static void Fill(string mySecretValue, string insertQuery)
-        {
-            using NpgsqlConnection connection = new NpgsqlConnection(mySecretValue);
-            connection.Open();
-
-            using NpgsqlCommand command = new NpgsqlCommand(insertQuery, connection);
-            command.ExecuteNonQuery();
-        }*/
         public static void Fill<T>(string mySecretValue, List<T> items, string tableName) where T : class
         {
             using NpgsqlConnection connection = new NpgsqlConnection(mySecretValue);
@@ -49,101 +41,38 @@ namespace BookUniverseConsole
             }
         }
 
-
-
         public static void GenerateData(string mySecretValue, int count)
         {
             Randomizer.Seed = new Random(54815148);
 
             var categoryFaker = DataGenerator.GetCategoryRule();
             var category = categoryFaker.Generate(count);
-            /*string sqlScript = "INSERT INTO \"Category\" (category_id, category_name) VALUES ";
-            for (int i = 0; i < count; i++)
-            {
-                sqlScript += $"({category[i].Id}, \'{category[i].CategoryName}\')";
-                sqlScript += (i < count - 1) ? ", " : ";";
-            }
-            Fill(mySecretValue, sqlScript);*/
             Fill(mySecretValue, category, "Category");
 
             var userFaker = DataGenerator.GetUserRule();
             userFaker.UseSeed(0);
             var user = userFaker.Generate(count);
-            /* sqlScript = "INSERT INTO \"User\" (user_id, username, email, password, role) VALUES ";
-             for (int i = 0; i < count; i++)
-             {
-                 int param1 = user[i].Id;
-                 string param2 = user[i].UserName;
-                 string param3 = user[i].Email;
-                 string param4 = user[i].Password;
-                 int param5 = user[i].Role;
-                 sqlScript += $"({param1}, \'{param2}\', \'{param3}\', \'{param4}\', {param5})";
-                 sqlScript += (i < count - 1) ? ", " : ";";
-             }
-             Fill(mySecretValue, sqlScript);*/
             Fill(mySecretValue, user, "User");
 
 
             var bookFaker = DataGenerator.GetBookRule(count);
             var book = bookFaker.Generate(count);
-            /*sqlScript = "INSERT INTO \"Book\" (book_id, title, author, description, number_of_pages, rating, category_id) VALUES ";
-            for (int i = 0; i < count; i++)
-            {
-                int param1 = book[i].Id;
-                string param2 = book[i].Title;
-                string param3 = book[i].Author;
-                string param4 = book[i].Description;
-                int param5 = book[i].NumberOfPages;
-                double param6 = book[i].Rating;
-                int param7 = book[i].CategoryId;
-                sqlScript += $"({param1}, \'{param2}\', \'{param3}\', \'{param4}\', {param5}, {param6.ToString(CultureInfo.InvariantCulture)}, {param7})";
-                sqlScript += (i < count - 1) ? ", " : ";";
-            }
-            Fill(mySecretValue, sqlScript);*/
             Fill(mySecretValue, book, "Book");
 
             var folderFaker = DataGenerator.GetFolderRule(count);
             var folder = folderFaker.Generate(count);
-            /*sqlScript = "INSERT INTO \"Folder\" (folder_id, folder_name, user_id) VALUES ";
-            for (int i = 0; i < count; i++)
-            {
-                sqlScript += $"({folder[i].Id}, \'{folder[i].FolderName}\', {folder[i].UserId})";
-                sqlScript += (i < count - 1) ? ", " : ";";
-            }
-            Fill(mySecretValue, sqlScript);*/
             Fill(mySecretValue, folder, "Folder");
 
             var userBookFaker = DataGenerator.GetUserBookRule(count);
             var userBook = userBookFaker.Generate(count);
-            /*sqlScript = "INSERT INTO \"UserBook\" (id, user_id, book_id) VALUES ";
-            for (int i = 0; i < count; i++)
-            {
-                sqlScript += $"({userBook[i].Id}, {userBook[i].UserId}, {userBook[i].BookId})";
-                sqlScript += (i < count - 1) ? ", " : ";";
-            }
-            Fill(mySecretValue, sqlScript);*/
             Fill(mySecretValue, userBook, "UserBook");
 
             var favouritesFaker = DataGenerator.GetFavouritesRule(count);
             var favourites = favouritesFaker.Generate(count);
-            /*sqlScript = "INSERT INTO \"Favourites\" (favorites_id, book_id, user_id) VALUES ";
-            for (int i = 0; i < count; i++)
-            {
-                sqlScript += $"({favourites[i].Id}, {favourites[i].BookId}, {favourites[i].UserId})";
-                sqlScript += (i < count - 1) ? ", " : ";";
-            }
-            Fill(mySecretValue, sqlScript);*/
             Fill(mySecretValue, favourites, "Favourites");
 
             var bookFolderFaker = DataGenerator.GetBookFolderRule(count);
             var bookFolder = bookFolderFaker.Generate(count);
-            /*sqlScript = "INSERT INTO \"BookFolder\" (id, book_id, folder_id) VALUES ";
-            for (int i = 0; i < count; i++)
-            {
-                sqlScript += $"({bookFolder[i].Id}, {bookFolder[i].BookId}, {bookFolder[i].FolderId})";
-                sqlScript += (i < count - 1) ? ", " : ";";
-            }
-            Fill(mySecretValue, sqlScript);*/
             Fill(mySecretValue, bookFolder, "BookFolder");
 
         }
