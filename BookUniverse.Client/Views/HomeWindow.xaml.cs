cde4f83261498ce976ce673710e86b8c07bb7795
KateSydor <katasydor@gmail.com>
2023-11-10 10:16:15 -0800

    Add list window

--- BookUniverse.Client/Views/HomeWindow.xaml.cs
+++ BookUniverse.Client/Views/HomeWindow.xaml.cs
@@ -71,7 +71,7 @@
 
         private void AccountButton_Click(object sender, RoutedEventArgs e)
         {
-            UserAccount userAccount = new UserAccount(_authenticationService, _userService);
+            ListOfBooks userAccount = new ListOfBooks(_authenticationService, _userService);
             this.Visibility = Visibility.Hidden;
             userAccount.Show();
         }
---
+++ BookUniverse.Client/Views/ListOfBooks.xaml.cs
@@ -0,0 +1,209 @@
+﻿namespace BookUniverse.Client
+{
+    using BookUniverse.Client;
+    using System;
+    using System.IO;
+    using System.Windows;
+    using BookUniverse.BLL.Interfaces;
+    using BookUniverse.DAL.Constants.UtilsConstants;
+    using BookUniverse.DAL.Entities;
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml.
+    /// </summary>
+    public partial class ListOfBooks : Window
+    {
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IUserService _userService;
+        private User currentUser;
+        private readonly PagingCollectionView _cview;
+
+
+        public ListOfBooks(IAuthenticationService authenticationService, IUserService userService)
+        {
+            _authenticationService = authenticationService;
+            _userService = userService;
+
+            Loaded += ListOfBooks_Loaded;
+
+            this.DataContext = currentUser;
+            InitializeComponent();
+            this._cview = new PagingCollectionView(
+                new List<object>
+                {
+                    new { Numer = "1", Title = "Cat", Author = "KateSydor"},
+                    new { Numer = "2", Title = "Cat2", Author = "KateSydor"}
+                },
+                2
+            );
+            this.DataContext = this._cview;
+        }
+
+        private async void ListOfBooks_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(UtilsConstants.FILE_PATH);
+                if (lines.Length >= 2)
+                {
+                    int userId = int.Parse(lines[0]);
+                    string userEmail = lines[1];
+
+                    currentUser = await _userService.GetUser(userEmail);
+                    username.Text = currentUser.Username;
+                }
+                else
+                {
+                    throw new Exception(UtilsConstants.FILE_ERROR);
+                }
+            }
+            catch
+            {
+                SignInWindow signInPage = new SignInWindow(_authenticationService, _userService);
+                signInPage.Show();
+                Hide();
+            }
+        }
+
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonLogout_Click(object sender, RoutedEventArgs e)
+        {
+            _authenticationService.Logout();
+            SignInWindow signInPage = new SignInWindow(_authenticationService, _userService);
+            signInPage.Show();
+            Hide();
+        }
+
+        private void AccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserAccount userAccount = new UserAccount(_authenticationService, _userService);
+            this.Visibility = Visibility.Hidden;
+            userAccount.Show();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            HomeWindow homeWindow = new HomeWindow(_authenticationService, _userService);
+            this.Visibility = Visibility.Hidden;
+            homeWindow.Show();
+        }
+
+        private void OnNextClicked(object sender, RoutedEventArgs e)
+        {
+            this._cview.MoveToNextPage();
+        }
+
+        private void OnPreviousClicked(object sender, RoutedEventArgs e)
+        {
+            this._cview.MoveToPreviousPage();
+        }
+    }
+    public class PagingCollectionView : CollectionView
+    {
+        private readonly IList _innerList;
+        private readonly int _itemsPerPage;
+
+        private int _currentPage = 1;
+
+        public PagingCollectionView(IList innerList, int itemsPerPage)
+            : base(innerList)
+        {
+            this._innerList = innerList;
+            this._itemsPerPage = itemsPerPage;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                if (this._innerList.Count == 0) return 0;
+                if (this._currentPage < this.PageCount) // page 1..n-1
+                {
+                    return this._itemsPerPage;
+                }
+                else // page n
+                {
+                    var itemsLeft = this._innerList.Count % this._itemsPerPage;
+                    if (0 == itemsLeft)
+                    {
+                        return this._itemsPerPage; // exactly itemsPerPage left
+                    }
+                    else
+                    {
+                        // return the remaining items
+                        return itemsLeft;
+                    }
+                }
+            }
+        }
+
+        public int CurrentPage
+        {
+            get { return this._currentPage; }
+            set
+            {
+                this._currentPage = value;
+                this.OnPropertyChanged(new PropertyChangedEventArgs("CurrentPage"));
+            }
+        }
+
+        public int ItemsPerPage { get { return this._itemsPerPage; } }
+
+        public int PageCount
+        {
+            get
+            {
+                return (this._innerList.Count + this._itemsPerPage - 1)
+                    / this._itemsPerPage;
+            }
+        }
+
+        private int EndIndex
+        {
+            get
+            {
+                var end = this._currentPage * this._itemsPerPage - 1;
+                return (end > this._innerList.Count) ? this._innerList.Count : end;
+            }
+        }
+
+        private int StartIndex
+        {
+            get { return (this._currentPage - 1) * this._itemsPerPage; }
+        }
+
+        public override object GetItemAt(int index)
+        {
+            var offset = index % (this._itemsPerPage);
+            return this._innerList[this.StartIndex + offset];
+        }
+
+        public void MoveToNextPage()
+        {
+            if (this._currentPage < this.PageCount)
+            {
+                this.CurrentPage += 1;
+            }
+            this.Refresh();
+        }
+
+        public void MoveToPreviousPage()
+        {
+            if (this._currentPage > 1)
+            {
+                this.CurrentPage -= 1;
+            }
+            this.Refresh();
+        }
+    }
+}
