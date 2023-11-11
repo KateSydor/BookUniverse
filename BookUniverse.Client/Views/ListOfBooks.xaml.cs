97a5e7ee4420d75604be99d32ec5ffe1a00ff3c0
KateSydor <katasydor@gmail.com>
2023-11-11 07:09:42 -0800

    Add list of books window

--- BookUniverse.Client/Views/ListOfBooks.xaml.cs
+++ BookUniverse.Client/Views/ListOfBooks.xaml.cs
@@ -20,7 +20,6 @@
         private readonly IAuthenticationService _authenticationService;
         private readonly IUserService _userService;
         private User currentUser;
-        private readonly PagingCollectionView _cview;
 
 
         public ListOfBooks(IAuthenticationService authenticationService, IUserService userService)
@@ -31,16 +30,16 @@
             Loaded += ListOfBooks_Loaded;
 
             this.DataContext = currentUser;
+            var bookList = new List<object>
+        {
+            new { Number = "1", Title = "Cat", Author = "KateSydor" },
+            new { Number = "2", Title = "Cat2", Author = "KateSydor" },
+            new { Number = "3", Title = "Cat3", Author = "KateSydor" },
+        };
+
             InitializeComponent();
-            this._cview = new PagingCollectionView(
-                new List<object>
-                {
-                    new { Numer = "1", Title = "Cat", Author = "KateSydor"},
-                    new { Numer = "2", Title = "Cat2", Author = "KateSydor"}
-                },
-                2
-            );
-            this.DataContext = this._cview;
+            dataGrid.ItemsSource = bookList;
+
         }
 
         private async void ListOfBooks_Loaded(object sender, RoutedEventArgs e)
@@ -96,114 +95,6 @@
             HomeWindow homeWindow = new HomeWindow(_authenticationService, _userService);
             this.Visibility = Visibility.Hidden;
             homeWindow.Show();
-        }
-
-        private void OnNextClicked(object sender, RoutedEventArgs e)
-        {
-            this._cview.MoveToNextPage();
-        }
-
-        private void OnPreviousClicked(object sender, RoutedEventArgs e)
-        {
-            this._cview.MoveToPreviousPage();
-        }
-    }
-    public class PagingCollectionView : CollectionView
-    {
-        private readonly IList _innerList;
-        private readonly int _itemsPerPage;
-
-        private int _currentPage = 1;
-
-        public PagingCollectionView(IList innerList, int itemsPerPage)
-            : base(innerList)
-        {
-            this._innerList = innerList;
-            this._itemsPerPage = itemsPerPage;
-        }
-
-        public override int Count
-        {
-            get
-            {
-                if (this._innerList.Count == 0) return 0;
-                if (this._currentPage < this.PageCount) // page 1..n-1
-                {
-                    return this._itemsPerPage;
-                }
-                else // page n
-                {
-                    var itemsLeft = this._innerList.Count % this._itemsPerPage;
-                    if (0 == itemsLeft)
-                    {
-                        return this._itemsPerPage; // exactly itemsPerPage left
-                    }
-                    else
-                    {
-                        // return the remaining items
-                        return itemsLeft;
-                    }
-                }
-            }
-        }
-
-        public int CurrentPage
-        {
-            get { return this._currentPage; }
-            set
-            {
-                this._currentPage = value;
-                this.OnPropertyChanged(new PropertyChangedEventArgs("CurrentPage"));
-            }
-        }
-
-        public int ItemsPerPage { get { return this._itemsPerPage; } }
-
-        public int PageCount
-        {
-            get
-            {
-                return (this._innerList.Count + this._itemsPerPage - 1)
-                    / this._itemsPerPage;
-            }
-        }
-
-        private int EndIndex
-        {
-            get
-            {
-                var end = this._currentPage * this._itemsPerPage - 1;
-                return (end > this._innerList.Count) ? this._innerList.Count : end;
-            }
-        }
-
-        private int StartIndex
-        {
-            get { return (this._currentPage - 1) * this._itemsPerPage; }
-        }
-
-        public override object GetItemAt(int index)
-        {
-            var offset = index % (this._itemsPerPage);
-            return this._innerList[this.StartIndex + offset];
-        }
-
-        public void MoveToNextPage()
-        {
-            if (this._currentPage < this.PageCount)
-            {
-                this.CurrentPage += 1;
-            }
-            this.Refresh();
-        }
-
-        public void MoveToPreviousPage()
-        {
-            if (this._currentPage > 1)
-            {
-                this.CurrentPage -= 1;
-            }
-            this.Refresh();
         }
     }
 }
