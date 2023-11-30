58bdca8c50ba798a2036ef8c3ac24ec0d2814fb6
KateSydor <katasydor@gmail.com>
2023-11-30 08:51:58 -0800

    Add title validation

--- BookUniverse.DAL/Constants/ValidationConstants/BookValidationConstants.cs
+++ BookUniverse.DAL/Constants/ValidationConstants/BookValidationConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class BookValidationConstants
     {
-        public const int BOOKTITLE_MIN_LENGTH = 10;
+        public const int BOOKTITLE_MIN_LENGTH = 3;
         public const int BOOKTITLE_MAX_LENGTH = 100;
 
         public const int AUTHOR_MIN_LENGTH = 5;
